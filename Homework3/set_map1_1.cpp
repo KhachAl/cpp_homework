9721b1f7642dc040fd8d174715d77cde5f12a5ca
Alex Khachatryan <aleksart2003@gmail.com>
2023-12-26 22:04:28 +0400

    Homework3

---
+++ Homework3/set_map1_1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+
+int foo(const std::string& s) {
+    std::map<char, int> count;
+
+    for (char c : s) {
+        count[c]++;
+    }
+
+    for (int i = 0; i < s.length(); ++i) {
+        if (count[s[i]] == 1) {
+            return i;
+        }
+    }
+
+    return -1; 
+}
+
+int main() {
+    std::string input;
+    std::cin >> input;
+    std::cout << foo(input) << std::endl;
+    
+
+    return 0;
+}
